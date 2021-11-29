93eeb09cf5f0769c6a295fd7bb64143167ce72db
Daniel Wennberg <daniel.wennberg@gmail.com>
2021-11-28 16:36:53 -0800

    Update author and email

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 setuptools.setup(
     name="explorecourses",
-    version="1.0.6",
-    url="https://github.com/illiteratecoder/Explore-Courses-API",
-    author="Jeremy Ephron",
-    author_email="jeremye@stanford.edu",
+    version="2.0.0",
+    url="https://github.com/danielwe/explore-courses-api",
+    author="Jeremy Ephron, Daniel Wennberg",
+    author_email="jeremye@stanford.edu, daniel.wennberg@gmail.com",
     description="A Python API for Stanford Explore Courses",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -18,7 +18,7 @@ setuptools.setup(
     classifiers=(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
